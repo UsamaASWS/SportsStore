fc6dc4d7d3496e541f165df98149b27d4ae27fc3
usama khan <usama3275@gmail.com>
2021-09-12 21:36:57 +0500

    Add project files.

---
+++ SportsStore.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SportsStore.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SportsStore.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("757fca7a-46f7-4b88-8b5a-251451163fbe")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SportsStore.WebUI/Controllers/NavController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SportsStore.Domain.Abstract;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class NavController : Controller
+    {
+        // GET: Nav
+        //private IProductsRepository repository;
+        //public NavController(IProductsRepository repo)
+        //{
+        //    repository = repo;
+        //}
+        //public PartialViewResult Menu2()
+        //{
+        //    IEnumerable<string> categories = repository.Products
+        //    .Select(x => x.Category)
+        //    .Distinct()
+        //    .OrderBy(x => x);
+        //    return PartialView(categories);
+        //}
+        private IProductsRepository repository;
+        public NavController(IProductsRepository repo)
+        {
+            repository = repo;
+        }
+        public PartialViewResult Menu(string category = null)
+        {
+            ViewBag.SelectedCategory = category;
+            IEnumerable<string> categories = repository.Products.Select(x => x.Category).Distinct().OrderBy(x => x);
+            return PartialView(categories);
+        }
+
+    }
+}
+
+
+
+//public string Menu()
+//        {
+//            return "Hello from NavController";
+//        }
+//    }
+//}
